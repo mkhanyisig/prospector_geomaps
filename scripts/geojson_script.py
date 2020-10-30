6ce245dfa5e96398727afbc585510cf754cb90b8
Mkhanyisi Gamedze <mkhanyisi.g@gmail.com>
2020-10-30 14:13:56 -0400

    Add files via upload

--- scripts/geojson_script.py
+++ scripts/geojson_script.py
@@ -1,6 +1,6 @@
 """
 Prospector Geospatial Data
-Create GeoJSON
+Create GeoJSON object from USGS data
 
 """
 
@@ -52,7 +52,59 @@ def produce(sz):
     output.close()
 
 
+def countryJSON(country):
+    # loab object specifications from dataframe
+    cnt=df[df['country']==country]
+    print(country,"  shape: ",cnt.shape)
+
+    result = cnt.to_json(orient="records") #  index labels are not preserved with this encoding
+    parsed = json.loads(result)
+    #print("JSON :\n")
+    dump=json.dumps(parsed, indent=4)
+    #print("Object: \n",dump)
+
+    feature_arr=[]
+
+    # build each feature object in country
+    for obj in parsed:
+        #print("object: ",obj)
+        arr_obj={}
+        arr_obj.update({"type":"Feature"})
+        arr_obj.update({"properties":obj})
+
+        geometry={}
+        geometry.update({"type":"Point"})
+        geometry.update({"coordinates":[obj.get('longitude'),obj.get('latitude')]}) # needs fixing
+
+        arr_obj.update({"geometry":geometry})
+
+        feature_arr.append(arr_obj)
+
+    print("Total # items: ",len(feature_arr))
+
+    geojson_obj={}
+    geojson_obj.update({"type":"FeatureCollection"})
+    geojson_obj.update({"features":feature_arr})
+
+    fn=""
+    fn+=country+".json"
+    fn=fn.replace(" ","_")
+
+    # export file
+    output = open(fn, 'w')
+
+    output.write(json.dumps(geojson_obj, indent=2))
+
+    output.close()
+
 ########### main function
 
+## Test 1 -> produce GEOJSON from first n rows
 print("starting")
 produce(1000)
+
+
+# test 2 produce GeoJSON for country
+print("Test #2")
+nm="Belize"
+countryJSON(nm)
